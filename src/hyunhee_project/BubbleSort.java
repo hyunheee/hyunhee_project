24f0e541b3657ebf8e66f59d2bfcc45284031143
hyunheee <jjj9238@naver.com>
2018-04-16 23:26:42 +0900

    Add BubbleSort.java

---
+++ src/hyunhee_project/BubbleSort.java
@@ -0,0 +1,33 @@
+package hyunhee_project;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] intArr = {10,9,5,8,2,7,3,6,4,1,0};
+		System.out.println(Arrays.toString(bubbleSort(intArr)));
+	}
+	
+	public static int[] bubbleSort(int[] intArr) {
+		final int SORT_LENGTH = intArr.length;
+		int[] bubbleSort = intArr;
+		boolean isChecked;
+		
+		for(int i=0;i<SORT_LENGTH;i++) {
+			isChecked = true;
+			for(int j=1;j<SORT_LENGTH-i;j++) {		
+				if(bubbleSort[j-1]>=bubbleSort[j]) {
+					int temp = bubbleSort[j-1];
+					bubbleSort[j-1] = bubbleSort[j];
+					bubbleSort[j] = temp;
+					isChecked=false;
+				}
+			}
+			if(isChecked) {
+				break;
+			}
+		}
+		
+		return bubbleSort;
+	}
+}
