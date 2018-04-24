985b2a4b45b61fb29737e01163eb109243354ae6
hyunheee <jjj9238@naver.com>
2018-04-24 22:19:38 +0900

    Modified SortTimeChecking.java.

--- src/hyunhee_project/SortTimeChecking.java
+++ src/hyunhee_project/SortTimeChecking.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SortTimeChecking {
-
 	static BubbleSort bubble = new BubbleSort();
 	static InsertionSort insert = new InsertionSort();
 	static SelectionSort select = new SelectionSort();
@@ -13,9 +12,9 @@ public class SortTimeChecking {
 	public static void main(String[] args) {
 		Random random = new Random();
 		
-		int[] intArr = new int[10000];
+		int[] intArr = new int[100000];
 		for(int i=0;i<intArr.length;i++) {
-			intArr[i] = random.nextInt(10000)+1; 
+			intArr[i] = random.nextInt(100000)+1; 
 		}
 	
 		timeCheck(intArr);
@@ -23,41 +22,38 @@ public class SortTimeChecking {
 	}
 	
 	public static void timeCheck(int[] intArr) {
-		int[] test1 = intArr;
-		int[] test2 = intArr;
-		int[] test3 = intArr;
-		int[] test4 = intArr;
-		
+		int intLength = intArr.length;
 		long start;
 		long end;
 		
-		start = System.currentTimeMillis();
-		quick.quickSort(test1,0,test1.length-1);
-		end = System.currentTimeMillis();
-		System.out.println( "quickSort 실행 시간 : " + (end-start));
-	
-		start = System.currentTimeMillis();
-		insert.insertionSort(test2);
-		end = System.currentTimeMillis();
-		System.out.println( "insertSort 실행 시간 : " + (end-start));
+		int[] test1 =Arrays.copyOf(intArr, intLength);
+		int[] test2 =Arrays.copyOf(intArr, intLength);
+		int[] test3 =Arrays.copyOf(intArr, intLength);
+		int[] test4 =Arrays.copyOf(intArr, intLength);
 		
 		start = System.currentTimeMillis();
 		select.selectionSort(test3);
 		end = System.currentTimeMillis();
-		System.out.println( "selectSort 실행 시간 : " + (end-start));
+		System.out.println( "selectSort 실행 시간 : " + (end-start)/1000.0);
 
+		
+		start = System.currentTimeMillis();
+		quick.quickSort(test1,0,test1.length-1);
+		end = System.currentTimeMillis();
+		System.out.println( "quickSort 실행 시간 : " + (end-start)/1000.0);
+	
+		start = System.currentTimeMillis();
+		insert.insertionSort(test2);
+		end = System.currentTimeMillis();
+		System.out.println( "insertSort 실행 시간 : " + (end-start)/1000.0);
+		
+	
 		start = System.currentTimeMillis();
 		bubble.bubbleSort(test4);
 		end = System.currentTimeMillis();
-		System.out.println( "bubbleSort 실행 시간 : " + (end-start));
-		
+		System.out.println( "bubbleSort 실행 시간 : " + (end-start)/1000.0);
 
-		
-
-
-
-	
-		
+		System.out.println(Arrays.equals(test1, test2)&&Arrays.equals(test3, test4));
 
 	}
 }
